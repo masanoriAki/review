11e4c460a02469e0e3a4ad935365dd60b4a8bf2f
Masnori Aki <h27-0591@nlinks.top>
2021-10-10 19:30:00 +0900

    モデルのメソッド不要

--- lib/notifier/my_app_view_model.dart
+++ lib/notifier/my_app_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux_provider/main.dart';
-import 'package:flutter_redux_provider/redux/action.dart';
 import 'package:flutter_redux_provider/redux/state.dart';
 import 'package:redux/redux.dart';
 
@@ -12,23 +11,5 @@ class MyAppViewModel extends ChangeNotifier {
     store.onChange.listen((event) {
       logger.info("state変わったよ");
     });
-  }
-
-  void addAge({@required int? age}) {
-    store.dispatch(AddAge(age!));
-    store.onChange.listen((event) => event.age!);
-    notifyListeners();
-  }
-
-  void addSex({@required int? sex}) {
-    store.dispatch(AddSex(sex!));
-    store.onChange.listen((event) => event.sex!);
-    notifyListeners();
-  }
-
-  void currentIndex({@required int? index}) {
-    store.dispatch(CurrentIndex(index!));
-    store.onChange.listen((event) => event.currentIndex);
-    notifyListeners();
   }
 }
